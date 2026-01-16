55939edbc7a625932d4e7978d8a1c31bbcbde97e
Deployment-SYS <ttestrsys@gmail.com>
2026-01-16 08:01:50 +0530

    changes

---
+++ AI-BC/day3.py
@@ -0,0 +1,36 @@
+#data type
+#Strings
+name = "Piyush"
+name1: str = "Piyush Saxena"
+
+print(name)
+print(type(name))
+print(name1)
+print(type(name1))
+
+#int
+age = 14
+#float
+age1 = 14.5
+
+print(age)
+print(type(age))
+print(age1)
+print(type(age1))
+
+age = "14"
+print(type(age))
+
+#boolean
+isAvailable = True
+print(isAvailable)
+print(type(isAvailable))
+
+#none
+isNone = None
+print(isNone)
+print(type(isNone))
+
+isNone = "updated value"
+print(isNone)
+print(type(isNone))
